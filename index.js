5446fc7e226851e4dfcca793b42a3fefbf419fb8
Manuel Beltran <manueldbeltranc@gmail.com>
2023-10-27 20:24:29 -0700

    change 404 handler

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ app.use(
 )
 
 // 404 handler
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', '404.html'))
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', '404.html'))
+// })
+app.use((req, res) => {
+  res.statusCode = 404
+  res.sendFile(path.resolve(__dirname, 'public', '404.html'))
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
